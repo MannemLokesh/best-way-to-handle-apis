a420dc0215558e9928ae21fadb3a0886734d0b93
MannemLokesh <110220365+MannemLokesh@users.noreply.github.com>
2024-07-23 19:59:40 +0530

    Create ApiError.java

---
+++ ApiError.java
@@ -0,0 +1,31 @@
+public class ApiError {
+    private String errorCode;
+    private String errorMessage;
+    private List<String> details;
+
+    // Getters and Setters
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public List<String> getDetails() {
+        return details;
+    }
+
+    public void setDetails(List<String> details) {
+        this.details = details;
+    }
+}
